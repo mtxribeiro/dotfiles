7248595b8d24e481fb799845c1a2ccadde10bac1
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-27 18:21:06 -0300

    Delete .config/fish/functions/la.fish

--- .config/fish/functions/la.fish
+++
@@ -1,3 +0,0 @@
-function la --wraps=ls --wraps='eza --color=always --all --git --no-filesize --icons=always --no-time --no-user --no-permissions' --description 'alias la=eza --color=always --all --git --no-filesize --icons=always --no-time --no-user --no-permissions'
-    eza --color=always --all --git --no-filesize --icons=always --no-time --no-user --no-permissions $argv
-end
