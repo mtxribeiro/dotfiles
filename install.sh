614af2a9dfcec29385d171b87fe7c9beeeaf31f2
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-26 23:32:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ set xdg_pkgs xdg-desktop-portal-hyprland xdg-user-dirs
 set gnome_pkgs polkit-gnome gnome-keyring gvfs
 set fonts_pkgs noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-font-awesome ttf-jetbrains-mono-nerd
 set media_pkgs mpv imv pavucontrol
-set tools_pkgs base-devel flatpak curl zip unzip unrar zoxide fzf neovim
+set tools_pkgs base-devel flatpak curl zip unzip unrar zoxide fzf eza neovim
 set gui_pkgs hyprland hyprpaper hyprlock waybar rofi nwg-bar
-set theme_pkgs materia-gtk-theme papirus-icon-theme
+set appearance_pkgs materia-gtk-theme papirus-icon-theme
 set softwares_pkgs firefox kitty pcmanfm
 
 sudo pacman -S --needed --noconfirm \
 	$audio_pkgs $codecs_pkgs $toolkits_pkgs $xdg_pkgs $gnome_pkgs \
-	$fonts_pkgs $media_pkgs $tools_pkgs $gui_pkgs $softwares_pkgs $theme_pkgs
+	$fonts_pkgs $media_pkgs $tools_pkgs $gui_pkgs $softwares_pkgs $appearance_pkgs
 
 if not type -q paru
     git clone https://aur.archlinux.org/paru.git /tmp/paru
