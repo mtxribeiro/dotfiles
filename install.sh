c8f76af6ba8cbd9608f677d49e37e8b38b9dc0d1
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-27 10:43:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,26 +2,25 @@
 
 sudo pacman -Syu --noconfirm
 
-set audio_pkgs pipewire-audio
-set codecs_pkgs gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
-set toolkits_pkgs gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland
-set xdg_pkgs xdg-desktop-portal-hyprland xdg-user-dirs
-set gnome_pkgs polkit-gnome gnome-keyring gvfs
-set fonts_pkgs noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-font-awesome ttf-jetbrains-mono-nerd
-set media_pkgs mpv imv pavucontrol
-set tools_pkgs base-devel flatpak curl zip unzip unrar zoxide fzf eza neovim
-set gui_pkgs hyprland hyprpaper hyprlock waybar rofi nwg-bar
-set themes_pkgs materia-gtk-theme papirus-icon-theme
-set softwares_pkgs firefox kitty pcmanfm
-
-sudo pacman -S --needed --noconfirm \
-	$audio_pkgs $codecs_pkgs $toolkits_pkgs $xdg_pkgs $gnome_pkgs \
-	$fonts_pkgs $media_pkgs $tools_pkgs $gui_pkgs $themes_pkgs $softwares_pkgs
+set pkgs \
+    pipewire-audio \
+    gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
+    gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
+    xdg-desktop-portal-hyprland xdg-user-dirs \
+    polkit-gnome gnome-keyring gvfs \
+    noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-font-awesome ttf-jetbrains-mono-nerd \
+    mpv imv pavucontrol \
+    base-devel flatpak curl zip unzip unrar zoxide fzf eza neovim \
+    hyprland hyprpaper hyprlock waybar rofi nwg-bar \
+    materia-gtk-theme papirus-icon-theme \
+    firefox kitty pcmanfm
 
 if not type -q paru
     git clone https://aur.archlinux.org/paru.git /tmp/paru
     cd /tmp/paru; makepkg -si --noconfirm; cd -
 end
+
+sudo pacman -S --needed --noconfirm $pkgs
 
 paru -S --needed --noconfirm hyprshot neofetch
 
