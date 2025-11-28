da6f45df2b12e0b316b38965e7e193dc3f2279a3
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-28 00:15:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 sudo pacman -Syu --noconfirm
+
 sudo pacman -S --needed --noconfirm \
     pipewire-audio \
     gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
@@ -12,20 +14,26 @@ sudo pacman -S --needed --noconfirm \
     hyprland hyprpaper hyprlock waybar rofi nwg-bar \
     materia-gtk-theme papirus-icon-theme \
     firefox alacritty pcmanfm
+    
 if ! command -v paru >/dev/null 2>&1; then
     git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin
     cd /tmp/paru-bin
     makepkg -si --noconfirm
     cd -
 fi
+
 paru -S --needed --noconfirm hyprshot
+
 mkdir -p ~/.config ~/Imagens
 cp -r ~/dotfiles/.config/* ~/.config/
 cp -r ~/dotfiles/Wallpapers ~/Imagens/
 xdg-user-dirs-update
+
 sudo pacman -Scc --noconfirm
+
 orphans=$(pacman -Qdtq || true)
 if [[ -n "$orphans" ]]; then
     sudo pacman -Rns --noconfirm $orphans
 fi
+
 echo -e "\n\033[1;32mInstalação concluída. Reinicie o sistema.\033[0m"
