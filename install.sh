0bb1d5ad1a59afce88487d7ebdca25885523c4b3
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-28 00:08:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,16 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm \
     pipewire-audio \
     gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
+    mpv imv pavucontrol \
     gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
     xdg-desktop-portal-hyprland xdg-user-dirs \
     polkit-gnome gnome-keyring gvfs \
     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
-    mpv imv pavucontrol \
     base-devel flatpak zip unzip unrar vim \
     hyprland hyprpaper hyprlock waybar rofi nwg-bar \
     materia-gtk-theme papirus-icon-theme \
     firefox alacritty pcmanfm
+
 
 if ! command -v paru >/dev/null 2>&1; then
     git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin
