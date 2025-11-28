d810b2e0c381aef447689b91e073617a4d4515c8
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-27 23:18:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set pkgs \
     polkit-gnome gnome-keyring gvfs \
     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
     mpv imv pavucontrol \
-    base-devel flatpak curl zip unzip unrar zoxide fzf neovim fastfetch \
+    base-devel flatpak zip unzip unrar zoxide fzf neovim \
     hyprland hyprpaper hyprlock waybar rofi nwg-bar \
     materia-gtk-theme papirus-icon-theme \
     firefox kitty pcmanfm
