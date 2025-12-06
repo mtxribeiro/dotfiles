e073eeaadc27f53882225fb8abc07e2dcc839d0f
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-12-06 11:31:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,27 @@
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --needed --noconfirm \
-    pipewire-audio gstreamer gst-plugins-{base,good,bad,ugly} gst-libav \
+    pipewire-audio \
+    gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
     mpv imv pavucontrol \
-    gtk{3,4} qt{5,6}-wayland xorg-xwayland \
+    gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
     xdg-desktop-portal-hyprland xdg-user-dirs \
     polkit-gnome gnome-keyring gvfs \
-    noto-fonts{,-emoji,-cjk} ttf-liberation ttf-jetbrains-mono-nerd \
+    noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
     base-devel flatpak zip unzip unrar vim \
     hyprland hyprpaper hyprlock waybar rofi \
     materia-gtk-theme papirus-icon-theme \
     firefox alacritty pcmanfm
 
-printf ":: Existem 2 opções disponíveis para AUR helper:\n"
-printf "   1) paru   2) yay\n\n"
-read -rp "Digite um número (default=1): " o
-o=${o:-1}
+o=""
+while [[ "$o" != 1 && "$o" != 2 ]]; do
+    printf ":: Existem 2 opções disponíveis para AUR helper:\n"
+    printf " 1) paru   2) yay\n\n"
+    read -rp "Digite um número (default=1): " o
+    o=${o:-1}
+done
 
-AUR_HELPER=$( [[ $o == 1 ]] && echo paru || echo yay )
+AUR_HELPER=$([[ $o == 1 ]] && echo paru || echo yay)
 
 if ! command -v "$AUR_HELPER" &>/dev/null; then
     echo ":: Instalando $AUR_HELPER..."
@@ -30,13 +34,19 @@ fi
 $AUR_HELPER -S --needed --noconfirm hyprshot
 
 mkdir -p ~/.config ~/Imagens
+
 cp -r ~/dotfiles/.config/* ~/.config/
 cp -r ~/dotfiles/Wallpapers ~/Imagens/
-cp ~/dotfiles/.{bashrc,bash_profile} ~/
+cp ~/dotfiles/.bashrc ~/.bashrc
+cp ~/dotfiles/.bash_profile ~/.bash_profile
 
 xdg-user-dirs-update
 
 sudo pacman -Scc --noconfirm
-orphans=$(pacman -Qdtq || true); [[ $orphans ]] && sudo pacman -Rns --noconfirm $orphans
 
-echo -e "\n\033[1;32m:: Instalação concluída. Reinicie o sistema.\033[0m"
+orphans=$(pacman -Qdtq || true)
+if [[ -n "$orphans" ]]; then
+    sudo pacman -Rns --noconfirm $orphans
+fi
+
+echo -e "\n\033[1;32mInstalação concluída. Reinicie o sistema.\033[0m"
