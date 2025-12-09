06ffc123c33e3a277286622225d33c0196974c0d
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-12-09 17:41:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ sudo pacman -Syu --noconfirm
 
 sudo pacman -S --needed --noconfirm \
     pipewire-audio pavucontrol \
-    gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
+    gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav mpv imv \
     gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
     xdg-desktop-portal-hyprland xdg-user-dirs \
     polkit-gnome gnome-keyring gvfs \
     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
-    base-devel flatpak zip unzip unrar mpv imv vim \
+    base-devel flatpak zip unzip unrar vim \
     hyprland hyprpaper hyprlock waybar rofi nwg-bar \
     materia-gtk-theme papirus-icon-theme \
     firefox alacritty pcmanfm
