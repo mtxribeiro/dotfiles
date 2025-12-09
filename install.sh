2de6fa47682af2f7818e6f7deb8add1fa130e3a6
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-12-09 17:28:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo pacman -S --needed --noconfirm \
     polkit-gnome gnome-keyring gvfs \
     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
     base-devel flatpak zip unzip unrar mpv imv vim \
-    ly hyprland hyprpaper hyprlock waybar rofi \
+    ly hyprland hyprpaper hyprlock waybar rofi nwg-bar \
     materia-gtk-theme papirus-icon-theme \
     firefox alacritty pcmanfm
 
