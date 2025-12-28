98c30bc74ac5f0eedb0a31e6adf0e103ff98dfe0
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-12-28 11:37:12 -0300

    Delete .config/fish/functions directory

--- .config/fish/functions/rm.fish
+++
@@ -1,3 +0,0 @@
-function rm --description 'alias rm=rm -r'
-    command rm -r $argv
-end
--- .config/fish/functions/sp.fish
+++
@@ -1,3 +0,0 @@
-function sp --wraps='sudo pacman' --description 'alias sp=sudo pacman'
-    sudo pacman $argv
-end
